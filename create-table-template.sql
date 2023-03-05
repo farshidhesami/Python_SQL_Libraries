660ebc72727ea2a1b2ea6883518fdab2f187e454
farshid hesasmi <54210997+farshidhesami@users.noreply.github.com>
2023-03-05 15:13:11 +0330

    Add files via upload

---
+++ create-table-template.sql
@@ -0,0 +1,16 @@
+-- Active: 1673294867357@@127.0.0.1@3306@test
+CREATE TABLE t1 (x1 VARCHAR(40) , x2 INT , x3 VARCHAR(60) , x4 VARCHAR(90) );
+
+select * from t1; 
+
+INSERT INTO t1 VALUES('farshid' , 345 ,'Hesami' ,'ineuron');
+
+create Table fsds (Student_name VARCHAR(50) , email_id VARCHAR(90) ,phone_number INT);
+
+
+INSERT into fsds VALUES('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875),('Farshid_Hesami', 'farshidhesami@gmail.com' , 091233875)
+
+select * from fsds;
+
+SHOW DATABASES;
+
